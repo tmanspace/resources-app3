e99c3bbda2ab24c03228a115354836454e75d1bc
Tim <serazutdinov.tim@gmail.com>
2022-03-03 05:35:55 +0300

    BaseBadge

---
+++ src/components/UI/BaseBadge.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="badge">
+  <a :href="badge.link">
+    {{badge.title}}
+  </a>
+</div>
+</template>
+
+<script>
+export default {
+  name: "BaseBadge",
+  props: ['badge']
+}
+</script>
+
+<style scoped>
+
+</style>
