e331e5ef0782bb8769f410ee60c09c9d64098a00
Tim <serazutdinov.tim@gmail.com>
2022-03-03 05:39:23 +0300

    Login page

---
+++ src/components/pages/LoginPage.vue
@@ -0,0 +1,29 @@
+<template>
+<section id="login">
+  <div class="input-modal">
+    <h3>Login</h3>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+  name: "LoginPage"
+}
+</script>
+
+<style scoped>
+#login {
+  width: 100%;
+  height: 100%;
+  display: flex;
+}
+
+.input-modal {
+  width: 300px;
+  height: 300px;
+  background: #fff;
+  border-radius: 10px;
+  box-shadow: #44424a;
+}
+</style>
